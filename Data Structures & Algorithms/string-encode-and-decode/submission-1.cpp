8fb8b167cd5ba9108ce4b9e2a490f59b19b22e63
Nethmika Kumarasiri <69431559+Nethmika4881@users.noreply.github.com>
2026-03-30 15:28:04 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+         
+        string encoded = "";
+        for(string w : strs){
+              encoded += (to_string(w.length())+"#"+w);
+        }
+        return encoded;
+
+    }
+
+    vector<string> decode(string s) {
+        vector<string> arr;
+        int i = 0 ;
+        
+        while(i < s.length()){
+            int j = i;
+            while(s[j] != '#'){
+                j+=1;
+            }
+
+
+            int size = stoi(s.substr(i,j-i));
+             i = j+1;
+
+            string word = s.substr(i,size);
+
+            arr.push_back(word);
+
+            i+=size;
+        }
+
+
+        return arr;
+
+    }
+};
