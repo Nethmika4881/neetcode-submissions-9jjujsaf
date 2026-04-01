28132244889c001c83da790ba5367f405293056b
Nethmika Kumarasiri <69431559+Nethmika4881@users.noreply.github.com>
2026-04-01 08:24:02 +0530

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    unordered_map<int,int> m;
+    int minCostClimbingStairs(vector<int>& cost) {
+       int size = cost.size();
+
+        for(int i=2;i<cost.size();i++){
+            cost[i] = min(cost[i-1],cost[i-2]) + cost[i];
+        }
+
+        for(int i : cost){
+            cout << i << endl;
+        }
+
+
+        return min(cost[size-1],cost[size-2]);
+    }
+};
