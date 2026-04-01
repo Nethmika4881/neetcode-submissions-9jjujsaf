8720134268c426ed6b57a239a22c94e250ec05e2
Nethmika Kumarasiri <69431559+Nethmika4881@users.noreply.github.com>
2026-04-01 09:46:51 +0530

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+      int size = nums.size();
+      
+
+      nums[0]=nums[0];
+      nums[1]=max(nums[0],nums[1]);
+      for(int i = 2 ; i < size;i++){
+        nums[i] = max(nums[i-2]+nums[i],nums[i-1]);
+
+      }  
+
+ 
+   
+      return max(nums[size-1],nums[size-2]);
+    }
+};
