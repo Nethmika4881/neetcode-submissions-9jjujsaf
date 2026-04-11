a1c373c416b44a7966a49187c65d2099f21675ba
Nethmika Kumarasiri <69431559+Nethmika4881@users.noreply.github.com>
2026-04-11 20:42:02 +0530

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.cpp
@@ -0,0 +1,48 @@
+#include<unordered_set>
+#include<queue>
+class Solution {
+public:
+    set<pair<int,int>> visited;
+    
+    void bfs(int i,int j,vector<vector<char>>& grid){
+            int rows = grid.size();
+            int cols = grid[0].size();
+            queue<pair<int,int>> q;
+            vector<pair<int,int>> directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+            q.push({i,j});
+
+            while(!q.empty()){
+                pair<int,int> p = q.front();
+                q.pop();
+                int x = p.first;
+                int y = p.second;
+                for(pair<int,int> d :  directions){
+                    int deltaX = d.first;
+                    int deltaY = d.second;
+
+                    if( 0<= (x+deltaX) && (x+deltaX) < rows && 0<= (y+deltaY)  && (y+deltaY) < cols && !visited.contains({x+deltaX,y+deltaY}) && grid[x+deltaX][y+deltaY] == '1'){
+                        visited.insert({x+deltaX,y+deltaY});
+                        q.push({x+deltaX,y+deltaY});
+                    }
+                }
+            }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int numOfIslands = 0;
+        int rows  = grid.size();
+        int cols = grid[0].size();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j] == '1' && !visited.contains({i,j})){
+                    visited.insert({i,j});
+                    bfs(i,j,grid);
+                    numOfIslands++;
+                }
+            }
+        }
+
+
+        return numOfIslands;
+    }
+};
